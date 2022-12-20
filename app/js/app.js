e6021bc139c70c526d44d2a7a802bbc1024a4507
VikashMaurya10 <vikashmaurya10stp@gmail.com>
2022-12-20 19:55:16 +0530

    .

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,17 @@
 $('.screen').click(() => {
     $('.widget').removeClass('add-widget');
+    $('.start').removeClass('add-start');
 });
 // prevent Tab key event
-// $(window).keydown((e) => {
-//     if (e.key === 'Tab') {
-//         e.preventDefault()
-//     }
-// });
+$(window).keydown((e) => {
+    if (e.key === 'Tab') {
+        e.preventDefault()
+    }
+});
 // Generate Random background
 $(window).ready(() => {
     const num = Math.ceil(Math.random() * 14);
-    randImage = "../app/images/wallpaper/" + num + ".jpg";
+    randImage = "./app/images/wallpaper/" + num + ".jpg";
     $("main").css('background-image', "url(" + randImage + ")");
 });
 
@@ -24,15 +25,22 @@ setInterval(() => {
     $('.date').text(date.toLocaleDateString({
         year: 'numeric', month: 'long'
     }))
+    $('.Date').text(date.toLocaleDateString("en-GB", {
+        month: 'long', day: 'numeric'
+    }))
+
 }, 999);
 
 // toggle weather widget
 $('.weather').mouseover(() => {
     $('.widget').addClass('add-widget');
+    $('.start').removeClass('add-start');
 }).click(() => {
     $('.widget').toggleClass('add-widget');
+    $('.start').removeClass('add-start');
 }).mouseleave(() => {
     $('.widget').removeClass('add-widget');
+
 });
 $('.widget').mouseover(() => {
     $('.widget').addClass('add-widget');
@@ -40,17 +48,21 @@ $('.widget').mouseover(() => {
     $('.widget').removeClass('add-widget');
 })
 $(window).keydown((e) => {
-    if (e.key === 'w' && e.altKey) { /* w = 87*/
+    if (e.key === 'w') {/* && e.altKey) { /* w = 87*/
+        e.preventDefault();
         $('.widget').toggleClass('add-widget');
+        $('.start').removeClass('add-start');
     }
 });
-
+// Toggle Start Menu
 $('.window').click(() => {
     $('.start').toggleClass('add-start');
 
 })
-$(window).keydown((e) => {
-    if (e.altKey === true) {
+$(window).keyup((e) => {
+    if (e.keyCode === 18) {
+        e.preventDefault();
         $('.start').toggleClass('add-start');
+        $('.widget').removeClass('add-widget');
     }
 });
