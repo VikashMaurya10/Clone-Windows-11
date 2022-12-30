a2a31b335b33c0a3060812a54d11b2b98bdf7400
VikashMaurya10 <vikashmaurya10stp@gmail.com>
2022-12-31 00:43:05 +0530

    icon delete

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,10 @@
 $('.screen').click(() => {
     $('.widget').removeClass('add-widget');
     $('.start').removeClass('add-start');
+    $('.search-bar').removeClass('add-search-bar');
+
 });
+
 // prevent Tab key event
 $(window).keydown((e) => {
     if (e.key === 'Tab') {
@@ -35,12 +38,14 @@ setInterval(() => {
 $('.weather').mouseover(() => {
     $('.widget').addClass('add-widget');
     $('.start').removeClass('add-start');
+    $('.search-bar').removeClass('add-search-bar');
+
 }).click(() => {
     $('.widget').toggleClass('add-widget');
     $('.start').removeClass('add-start');
+    $('.search-bar').removeClass('add-search-bar');
 }).mouseleave(() => {
     $('.widget').removeClass('add-widget');
-
 });
 $('.widget').mouseover(() => {
     $('.widget').addClass('add-widget');
@@ -52,17 +57,61 @@ $(window).keydown((e) => {
         e.preventDefault();
         $('.widget').toggleClass('add-widget');
         $('.start').removeClass('add-start');
+        $('.search-bar').removeClass('add-search-bar');
     }
 });
 // Toggle Start Menu
 $('.window').click(() => {
     $('.start').toggleClass('add-start');
+    $('.search-bar').removeClass('add-search-bar');
+    $('.widget').removeClass('add-widget');
 
 })
 $(window).keyup((e) => {
     if (e.keyCode === 18) {
         e.preventDefault();
         $('.start').toggleClass('add-start');
+        $('.widget').removeClass('add-widget');
+        $('.search-bar').removeClass('add-search-bar');
+    }
+});
+
+//search bar 
+function leftScroll(container) {
+    $('.' + container).animate({ scrollLeft: "-=139px" }, "slow");
+}
+function rightScroll(container) {
+    $('.' + container).animate({ scrollLeft: "+=139px" }, "slow");
+}
+$('.l-btn1').click(function () {
+    leftScroll('Scroll')
+});
+$('.r-btn1').click(function () {
+    rightScroll('Scroll')
+});
+
+$('.L-btn2').click(function () {
+    leftScroll('Scroll2')
+});
+$('.R-btn2').click(function () {
+    rightScroll('Scroll2')
+});
+
+$('.search').click(function () {
+    $('.search-bar').toggleClass('add-search-bar');
+    $('.start').removeClass('add-start');
+    $('.widget').removeClass('add-widget');
+});
+$('.input').click(function (e) {
+    e.preventDefault();
+    $('.search-bar').addClass('add-search-bar');
+    $('.start').removeClass('add-start');
+});
+$(window).keydown((e) => {
+    if (e.key === 's') {/* && e.altKey) { /* w = 87*/
+        e.preventDefault();
+        $('.search-bar').toggleClass('add-search-bar');
+        $('.start').removeClass('add-start');
         $('.widget').removeClass('add-widget');
     }
 });
